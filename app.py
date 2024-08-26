8827072c0e1074202073d9136a6dcf071326635e
Poonam Kumari <98678504+Poonam-Kumari1210@users.noreply.github.com>
2024-08-26 16:49:08 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import requests
-
 import pandas as pd
 
 
@@ -10,73 +9,49 @@ def fetch_poster(movie_id):
         movie_id)
     data = requests.get(url)
     data = data.json()
-    print(data)
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    poster_path = data.get('poster_path')  # Use get to avoid KeyError
+    if poster_path:  # Check if poster_path exists
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+        return full_path
+    return None  # Return None if no poster found
 
 
 def recommend(movie):
     movies1 = pd.DataFrame(movies)
     movie_index = int(movies1.loc[movies1['title'] == movie].index[0])
-    # movie_index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[movie_index])), reverse=True, key=lambda x: x[1])
-    # movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        #movie_id = movies.iloc[i[0]].movie_id
-
+    for i in distances[1:6]:  # Get top 5 recommendations
         movie_id = movies1['movie_id'][i[0]]
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies['title'][i[0]])
+        poster = fetch_poster(movie_id)
+        if poster:  # Check if poster is valid
+            recommended_movie_posters.append(poster)
+            recommended_movie_names.append(movies['title'][i[0]])
 
     return recommended_movie_names, recommended_movie_posters
 
 
-st.header('MOVIES RECOMMENDER SYSTEM')
-movies = pickle.load(open('movie_list.pkl', 'rb'))
+# Load data safely
+try:
+    st.header('MOVIES RECOMMENDER SYSTEM')
+    movies = pickle.load(open('movie_list.pkl', 'rb'))
+    similarity = pickle.load(open('similarity_compressed.pkl', 'rb'))
 
-print(movies)
-similarity = pickle.load(open('similarity_compressed.pkl', 'rb'))
+    movie_list = list(movies['title'].values())
+    selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
 
-#movie_list = list(movies['title'].values)
-movie_list = list(movies['title'].values())
+    if st.button('Show Recommendation'):
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        for i, col in enumerate([col1, col2, col3, col4, col5]):
+            if i < len(recommended_movie_names):  # Check bounds
+                with col:
+                    st.text(recommended_movie_names[i])
+                    st.image(recommended_movie_posters[i])
 
-#movie_list = list(movies['title'].dropna().values)
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
-
-if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-# movies_dict = pickle.load(open('movie_list.pkl','rb'))
-# movies = pd.DataFrame(movies_dict)
-# similarity = pickle.load(open('similarity.pkl','rb'))
-# st.title('MOVIES RECOMMENDER SYSTEM')
-# selected_movie_name = st.selectbox("How would you like to be contacted?",movies['title'].values)
-#
-# if st.button("Recommend"):
-#     recommendations = recommend(selected_movie_name)
-#     for i in recommendations:
-#         st.write(i)
+except FileNotFoundError as e:
+    st.error(f"Error loading files: {e}")
+except Exception as e:
+    st.error(f"An unexpected error occurred: {e}")
